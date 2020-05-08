5840fb9460d3f89b6cebdf859f36e7a99ca8011f
Chriiiiiss <chris.fraanc@gmail.com>
2020-05-08 17:46:20 +0200

    :construction: [ADD] stroke system

--- scripts/main.js
+++ scripts/main.js
@@ -2,40 +2,18 @@ let _w = window.innerWidth
 let _h = window.innerHeight
 const container = document.querySelector(".container")
 const buttons = container.querySelectorAll("div[class^='button'")
-
+let currentAudio
+let amplitude
+let soundList
 class sound {
     constructor(id, url) {
         this.id = id,
         this.url = url
     }
 }
+let historyBeats  = []
 
-const soundList = [
-    new sound(0,"../sounds/aweyeah.mp3"),
-    new sound(1,"../sounds/drake-ugh.mp3"),
-    new sound(2,"../sounds/drake-yeauh.mp3"),
-    new sound(3,"../sounds/ha.mp3"),
-    new sound(4,"../sounds/hey.mp3"),
-    new sound(5,"../sounds/holdup.mp3"),
-    new sound(6,"../sounds/huh.mp3"),
-    new sound(7,"./sounds/jyea.mp3"),
-    new sound(8,"./sounds/khaled.mp3"),
-    new sound(9,"./sounds/ugh.mp3")
-]
-
-buttons.forEach(button => {
-    button.addEventListener("click", (e) => {
-        e.preventDefault
-        button.classList.remove("click_anim")
-        void button.offsetWidth // makes the button to reflow and reset the animation
-        button.classList.add("click_anim")
-        playAudio(soundList[button.attributes[1].value].url)
-        
-    })
-})
-
-function playAudio(url) {
-    const audio = new Audio(url)
+function playAudio(audio) {
     audio.play()
 }
 
@@ -45,10 +23,51 @@ window.addEventListener("resize", () => {
     resizeCanvas(_w, _h)
 })
 
+function preload() {
+    soundList = [
+        new sound(0,loadSound("../sounds/aweyeah.mp3")),
+        new sound(1,loadSound("../sounds/drake-ugh.mp3")),
+        new sound(2,loadSound("../sounds/drake-yeauh.mp3")),
+        new sound(3,loadSound("../sounds/ha.mp3")),
+        new sound(4,loadSound("../sounds/hey.mp3")),
+        new sound(5,loadSound("../sounds/holdup.mp3")),
+        new sound(6,loadSound("../sounds/huh.mp3")),
+        new sound(7,loadSound("./sounds/jyea.mp3")),
+        new sound(8,loadSound("./sounds/khaled.mp3")),
+        new sound(9,loadSound("./sounds/ugh.mp3"))
+    ]
+}
+
 function setup() {
     createCanvas(_w, _h)
+    buttons.forEach(button => {
+        button.addEventListener("click", (e) => {
+            e.preventDefault
+            button.classList.remove("click_anim")
+            void button.offsetWidth // makes the button to reflow and reset the animation
+            button.classList.add("click_anim")
+            currentAudio = soundList[button.attributes[1].value].url
+            playAudio(currentAudio)
+        })
+    })
+    amplitude = new p5.Amplitude()
 }
 
 function draw() {
     background(0)
+    let vol = amplitude.getLevel()
+    historyBeats.push(vol)
+    stroke(255)
+    noFill()
+    beginShape()
+    for (let i = 0; i < historyBeats.length; i++) {
+        let y = map(historyBeats[i], 0, 1, _h, 0)
+        vertex(i, y)
+    }
+    endShape()
+
+    if (historyBeats.length > _w) {
+        historyBeats.splice(0 , 1)
+    }
+    // ellipse(100, 100, 200, vol * 200)
 }
