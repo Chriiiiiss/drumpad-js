b9b618eff5da488b4f58124ee095229858d5552f
Chriiiiiss <chris.fraanc@gmail.com>
2020-05-10 05:33:55 +0200

    :recycle: refactoring code && add toggle switch input

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,29 @@
+// DOM ELEM
+
+const containerElem = document.querySelector(".container_pads")
+const buttonsElem = containerElem.querySelectorAll("div[class^='button'")
+const switchElem = document.querySelector(".checkbox")
+
+// UGLY GLOBAL VARS
+
 let _w = window.innerWidth
 let _h = window.innerHeight
-const container = document.querySelector(".container")
-const buttons = container.querySelectorAll("div[class^='button'")
 let currentAudio
 let amplitude
+let historyBeats  = []
+let vol 
+let fft
+let spectrum
 let soundList
 let radius = 0
+
+// SOME CONST (HAVE FUN 🥳)
+
 const MAX_DEGREE = 360
 const LINE_MAX_HEIGHT = 400
 const LINE_MIN_HEIGHT = 150
+
+// SOUND OBJECT
 
 class sound {
     constructor(id, url) {
@@ -16,17 +31,18 @@ class sound {
         this.url = url
     }
 }
-let historyBeats  = []
 
-function playAudio(audio) {
-    audio.play()
-}
+
+// AUTO RESIZE CANVAS
 
 window.addEventListener("resize", () => {
     _w = window.innerWidth
     _h = window.innerHeight
     resizeCanvas(_w, _h)
 })
+
+
+// PRELOAD P5 FUNCTION USED TO PRELOAD SOUNDS
 
 function preload() {
     soundList = [
@@ -44,9 +60,14 @@ function preload() {
     ]
 }
 
+// SETUP FUNCTION TO SETUP THE CANVAS
+// ADD A CLICK EVENT LISTENER TO PLAY SONGS (PERSONAL CHOICE TO NOT APPLY A KEYDOWN EVENT LISTENER)
+// USING offset.Width MAKES REFLOW THE BUTTONS AND RESET THE ANIMATION
+// USING A isLoop FUNCTION DESCRIBED LATER
+
 function setup() {
     createCanvas(_w, _h)
-    buttons.forEach(button => {
+    buttonsElem.forEach(button => {
         button.addEventListener("click", (e) => {
             e.preventDefault     
             button.classList.remove("click_anim")
@@ -59,47 +80,86 @@ function setup() {
                 currentAudio.stop()
             }
             if (currentAudio.isPlaying() === false && isLoop(button) === false) {
-                playAudio(currentAudio)
+                currentAudio.play()
             }        
         })
     })
+    fft = new p5.FFT(0, 256)
     amplitude = new p5.Amplitude()
 }
 
-function isLoop(button) {
-    return app = (button.classList[1] === "loop") ? true : false
-}
+// DRAW P5 LOOP FUNCTION
+// USING P5 FFT AND AMPLITUDE OBJECT
 
 function draw() {
     background(0)
+    spectrum = fft.analyze()
     colorMode(HSB)
     angleMode(DEGREES)
-    let vol = amplitude.getLevel()
+    vol = amplitude.getLevel()
     historyBeats.push(vol)
-    // fill(Math.random() * 255, Math.random() * 255, 0)
-    // noFill()
+    fill(Math.random() * 255, Math.random() * 255, 0)
+    noFill()
     translate(width / 2, height / 2)
     if (radius > 350) {
         background(270, 100, 37)
         
     }
     beginShape()
-    for (let i = 0; i < MAX_DEGREE ; i++) {
-        if (historyBeats[i]) {
-            radius = map(historyBeats[i], 0, 0.5, LINE_MIN_HEIGHT, LINE_MAX_HEIGHT)
-        } else {
-            radius = map(0, 0, 0.5, LINE_MIN_HEIGHT, LINE_MAX_HEIGHT)
-        }
-
-        let x = radius * cos(i)
-        let y = radius * sin(i)
-        stroke(i, 255, 255)
-        fill(i, 255, 255)
-        line(0,0, x, y)
+    if (switchElem.checked) {
+        fftRadial()
+        
+    } else {
+        ampRadial()
     }
     endShape()
     
     if (historyBeats.length > 360) {
         historyBeats.splice(0 , 1)
     }
+}
+
+
+// FFT RADIAL CIRCLE FUNCTION 
+
+function fftRadial() {
+    for (let i = 0; i < spectrum.length ; i++) {
+        let amp = spectrum[i]
+        let angle = map(i, 0, spectrum.length, 0,  MAX_DEGREE)
+        if (spectrum[i]) {
+            radius = map(amp, 0, 256, LINE_MIN_HEIGHT, LINE_MAX_HEIGHT) 
+        } else {
+            radius = map(0, 0, 256, LINE_MIN_HEIGHT, LINE_MAX_HEIGHT) 
+        }
+        let x = radius * cos(angle)
+        let y = radius * sin(angle)
+        stroke(i, 255, 255)
+        fill(i, 255, 255)
+        line(0, 0, x, y)
+    }
+}
+
+// AMPLITUDE RADIAL CIRCLE FUNCTION
+
+function ampRadial() {
+    for (let i = 0; i < MAX_DEGREE ; i++) {
+        if (historyBeats[i]) {
+            radius = map(historyBeats[i], 0, 0.5, LINE_MIN_HEIGHT, LINE_MAX_HEIGHT)
+        } else {
+            radius = map(0, 0, 0.5, LINE_MIN_HEIGHT, LINE_MAX_HEIGHT)
+        }
+        
+        let x = radius * cos(i)
+        let y = radius * sin(i)
+        stroke(i, 255, 255)
+        fill(i, 255, 255)
+        line (0,0, x, y)
+    }
+}
+
+
+// TERNARY FUNCTION THAT RETURNS A BOOLEAN
+
+function isLoop(button) {
+    return app = (button.classList[1] === "loop") ? true : false
 }
